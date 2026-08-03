8dc4d1e094f77b0ceb0c604ec6157d6a2d6ed4eb
Pankaj Bhise <bhisepankaj111@gmail.com>
2026-08-03 23:18:53 +0530

    End of Section 13

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -5,6 +5,7 @@ import { MatButton } from '@angular/material/button';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { BusyService } from '../../core/services/busy.service';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -21,4 +22,5 @@ import { BusyService } from '../../core/services/busy.service';
 })
 export class HeaderComponent {
   busyService = inject(BusyService)
+  cartService = inject(CartService)
 }
---
+++ client/src/app/shared/components/order-summary/order-summary.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { RouterLink } from "@angular/router";
+import { CartService } from '../../../core/services/cart.service';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-order-summary',
+  imports: [
+    RouterLink,
+     MatButton,
+      MatInput,
+      MatLabel,
+      MatFormField,
+      CurrencyPipe
+
+    ],
+  templateUrl: './order-summary.component.html',
+  styleUrl: './order-summary.component.scss',
+})
+export class OrderSummaryComponent {
+  cartService = inject(CartService);
+}
