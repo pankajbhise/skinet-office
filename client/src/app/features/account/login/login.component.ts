d2c4f4a3289739ddf16967f2078aa3db964bc0e0
Pankaj Bhise <bhisepankaj111@gmail.com>
2026-08-17 22:30:02 +0530

    End of Section 15

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -5,7 +5,7 @@ import { MatCard } from '@angular/material/card';
 import { MatInput } from '@angular/material/input';
 import { MatFormField, MatLabel } from '@angular/material/select';
 import { AccountService } from '../../../core/services/account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -24,6 +24,14 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
   private accountService = inject(AccountService);
   private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  returnUrl = '/shop';
+
+  constructor() {
+    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    if (url) this.returnUrl = url;
+  }
+
 
   loginForm = this.fb.group({
     email: [''],
@@ -34,7 +42,7 @@ export class LoginComponent {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
         this.accountService.getUserInfo().subscribe();
-        this.router.navigateByUrl('/shop');
+        this.router.navigateByUrl(this.returnUrl);
       }
     })
   }
