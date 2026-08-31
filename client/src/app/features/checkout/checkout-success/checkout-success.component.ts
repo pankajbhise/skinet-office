97c96fcab63ef6c34b0a8c348c7ee2d2842d5dfe
Pankaj Bhise <bhisepankaj111@gmail.com>
2026-08-31 12:07:17 +0530

    End of Section 18

--- client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { SignalrService } from '../../../core/services/signalr.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { AddressPipe } from '../../../shared/pipes/address-pipe';
+import { PaymentCardPipe } from '../../../shared/pipes/payment-card-pipe';
+import { OrderService } from '../../../core/services/order.service';
 
 @Component({
   selector: 'app-checkout-success',
   imports: [
     MatButton,
-    RouterLink
+    RouterLink,
+    MatProgressSpinnerModule,
+    DatePipe,
+    AddressPipe,
+    CurrencyPipe,
+    PaymentCardPipe
   ],
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss',
 })
 export class CheckoutSuccessComponent {
+  signalrService = inject(SignalrService);
+  private orderService = inject(OrderService);
+
+  ngOnDestroy(): void {
+    this.orderService.orderComplete = false;
+    this.signalrService.orderSignal.set(null);
+  }
 
 }
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:5001/api/',
+    hubUrl: 'https://localhost:5001/hub/notifications',
     stripePublicKey: 'pk_test_51U5pmmK9O5BL7Dc7vtlL7MR1IvrjfgRD4fvrJLCmaIDfaDgTKkeLRSX2Y9kGCZhcKZixPZTGsUuLqQc0aaHD1RIl00ldcx71ud'
 };
